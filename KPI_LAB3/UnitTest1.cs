20e395e0f8af32002922684cec1544e3ce8b039b
ykh2k <khilchenko.yehor@gmail.com>
2021-01-03 19:29:04 +0200

    feat: update

--- KPI_LAB3/UnitTest1.cs
+++ KPI_LAB3/UnitTest1.cs
@@ -2,42 +2,54 @@ using System;
 using Xunit;
 using IIG.PasswordHashingUtils;
 using System.Collections.Generic;
+using Xunit.Abstractions;
 
 namespace KPI_LAB3
 {
     public class UnitTest1
     {
+        private readonly ITestOutputHelper output;
+
+        public UnitTest1(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
         private static string GenerateReallyLongString()
         {
             string returnstring = "";
-            for (uint i = 0; i < /* uint.MaxValue / */ 100000; i++) returnstring += "a";
+            for (uint i = 0; i < 100000; i++) returnstring += "a";
             return returnstring;
         }
 
-        public static IEnumerable<object[]> GetData()
+        public static IEnumerable<object[]> GetDataHash()
         {
             string longstring = GenerateReallyLongString();
-            yield return new object[] { "", "", null };
-            yield return new object[] { "", "", (uint)10 };
-            yield return new object[] { "", "soul", null };
-            yield return new object[] { "", "soul", (uint)10 };
+            yield return new object[] { "'' 'password' + '' 'salt' + null 'adlerMod32'", "", "", null };
+            yield return new object[] { "'' 'password' + '' 'salt' + 10 'adlerMod32'", "", "", (uint)10 };
+            yield return new object[] { "'' 'password' + 'soul' 'salt' + null 'adlerMod32'", "", "soul", null };
+            yield return new object[] { "'' 'password' + 'soul' 'salt' + 10 'adlerMod32'", "", "soul", (uint)10 };
 
-            yield return new object[] { "password", "", null };
-            yield return new object[] { "password", "", (uint)10 };
-            yield return new object[] { "password", "soul", null };
-            yield return new object[] { "password", "soul", (uint)10 };
+            yield return new object[] { "'password' 'password' + '' 'salt' + null 'adlerMod32'", "password", "", null };
+            yield return new object[] { "'password' 'password' + '' 'salt' + 10 'adlerMod32'", "password", "", (uint)10 };
+            yield return new object[] { "'password' 'password' + 'soul' 'salt' + null 'adlerMod32'", "password", "soul", null };
+            yield return new object[] { "'password' 'password' + 'soul' 'salt' + 10 'adlerMod32'", "password", "soul", (uint)10 };
 
             // Long string for getting into catch statement
-            yield return new object[] { longstring, "", null };
-            yield return new object[] { longstring, "", (uint)10 };
-            yield return new object[] { longstring, "soul", null };
-            yield return new object[] { longstring, "soul", (uint)10 };
+            yield return new object[] { "longpassword 'password' + '' 'salt' + null 'adlerMod32'", longstring, "", null };
+            yield return new object[] { "longpassword 'password' + '' 'salt' + 10 'adlerMod32'", longstring, "", (uint)10 };
+            yield return new object[] { "longpassword 'password' + 'soul' 'salt' + null 'adlerMod32'", longstring, "soul", null };
+            yield return new object[] { "longpassword 'password' + 'soul' 'salt' + 10 'adlerMod32'", longstring, "soul", (uint)10 };
         }
 
         [Theory]
-        [MemberData(nameof(GetData))]
-        public void TestGetHashPassword(string password, string salt, uint? adlerMod32)
+        [MemberData(nameof(GetDataHash))]
+        public void TestGetHash(string description, string password, string salt, uint? adlerMod32)
         {
+            output.WriteLine("+=======================+");
+            output.WriteLine(description);
+            output.WriteLine("+=======================+");
+
             Assert.Equal(
                 PasswordHasher.GetHash(password, salt, adlerMod32),
                 PasswordHasher.GetHash(password, salt, adlerMod32)
@@ -46,6 +58,29 @@ namespace KPI_LAB3
                 PasswordHasher.GetHash(password, salt, adlerMod32),
                 PasswordHasher.GetHash(password+"1", salt, adlerMod32)
             );
+        }
+
+
+        public static IEnumerable<object[]> GetDataInit()
+        {
+            yield return new object[] { "null 'salt', 0 'adlerMod32'", "password", null, 0 };
+            yield return new object[] { "", "", "password", (uint)10 };
+            yield return new object[] { "", "soul", "password", null };
+            yield return new object[] { "", "soul", "password", (uint)10 };
+
+        }
+
+        [Theory]
+        [MemberData(nameof(GetDataInit))]
+        public void TestInit(string description, string password, string salt, uint adlerMod32)
+        {
+            output.WriteLine("+=======================+");
+            output.WriteLine(description);
+            output.WriteLine("+=======================+");
+            PasswordHasher.Init(salt, adlerMod32);
+            Assert.Equal(PasswordHasher.GetHash(password), PasswordHasher.GetHash(password));
+            Assert.NotEqual(PasswordHasher.GetHash(password), PasswordHasher.GetHash(password + "1"));
+
         }
     }
 }
